04b5d8b9df6e64dd525d689cb4da49a4bbeb2627
mariambenali <miriam.bena@gmail.com>
2024-11-25 22:44:08 +0100

    test

--- sms/settings.py
+++ sms/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'sms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'systeme_management_student',
+        'USER': 'miriambenali',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
