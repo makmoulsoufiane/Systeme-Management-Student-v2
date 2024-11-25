d091569e770bdd784c01a2d570bd65828c945812
makmoul soufiane <makmoulsoufiane2@gmail.com>
2024-11-25 22:07:43 +0100

    test connection database postsql

--- sms/settings.py
+++ sms/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'sms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'systeme_management_student',
+        'USER': 'soufianemakmoul',
+        'PASSWORD': 'Django1234',
+        'HOST': 'localhost',  # or the server IP if remote
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
