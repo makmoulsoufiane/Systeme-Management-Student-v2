2c86de961e249966981befc05ec18509ca06c863
makmoul soufiane <makmoulsoufiane2@gmail.com>
2024-12-01 11:59:04 +0100

    create app and class model student

--- sms/settings.py
+++ sms/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'sms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'systeme_management_student',
+        'USER': 'soufianemakmoul',
+        'PASSWORD': 'Django1234',
+        'HOST': 'localhost',  # or the server IP if remote
+        'PORT': '5432',
     }
 }
 
