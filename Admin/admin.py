58b5fb946a5eaed5f6167ebb38224ff22978f85c
mariambenali <miriam.bena@gmail.com>
2024-12-02 22:08:31 +0100

    create

--- Admin/admin.py
+++ Admin/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Admin
 
-# Register your models here.
+@admin.register(Admin)
+class AdminAdmin(admin.ModelAdmin):
+    
+    list_display = ('id_admin', 'fullname_admin', 'address', 'date_of_inscription')
+    
+    
+    
