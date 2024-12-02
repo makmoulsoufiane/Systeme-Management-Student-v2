8dd2f8595cbd5d435d8b9e3f4eb7dd10f3a417f0
mariambenali <miriam.bena@gmail.com>
2024-12-02 22:13:07 +0100

    add model to admin.py

--- Admin/admin.py
+++ Admin/admin.py
@@ -3,7 +3,7 @@ from .models import Admin
 
 @admin.register(Admin)
 class AdminAdmin(admin.ModelAdmin):
-    
+    ''' this list will display as a table'''
     list_display = ('id_admin', 'fullname_admin', 'address', 'date_of_inscription')
     
     
