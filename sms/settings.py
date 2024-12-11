aa1da39d0f664cba4c89d1baa65dd504e7d1af31
Boubaker17 <boubaker.benaissa@hotmail.fr>
2024-12-11 20:58:00 +0100

    add .env file in my project

--- sms/settings.py
+++ sms/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'sms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
@@ -83,6 +83,21 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
+
+from decouple import config
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
+    }
+}
+
 
 
 # Password validation
