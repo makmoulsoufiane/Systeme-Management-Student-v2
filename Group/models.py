46e121cd2e49896fd9a0b17d30d73395c74cc8f3
makmoul soufiane <makmoulsoufiane2@gmail.com>
2024-12-29 13:34:48 +0100

    fix code model group student many to many

--- Group/models.py
+++ Group/models.py
@@ -1,11 +1,14 @@
 # Group/models.py
 from django.db import models
+from student.models import Student
+
+
 
 class Group(models.Model):
     id_group = models.AutoField(primary_key=True)
     year_of_creation = models.PositiveIntegerField()
+    students = models.ManyToManyField(Student, related_name='groups')
     # علاقة Many-to-Many مع الطلاب (طالب يمكن أن ينتمي إلى عدة مجموعات)
-    students = models.ManyToManyField('Student', related_name='groups')
-
+    
     def __str__(self):
         return f"Group {self.id_group} - {self.year_of_creation}"
