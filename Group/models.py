1e2fe119f13f79a54c9926af1b97af79295d7e20
makmoul soufiane <makmoulsoufiane2@gmail.com>
2024-12-29 18:23:36 +0100

    one to many admin to groups

--- Group/models.py
+++ Group/models.py
@@ -1,6 +1,7 @@
 # Group/models.py
 from django.db import models
 from student.models import Student
+from Admin.models import Admin
 
 
 
@@ -8,7 +9,9 @@ class Group(models.Model):
     id_group = models.AutoField(primary_key=True)
     year_of_creation = models.PositiveIntegerField()
     students = models.ManyToManyField(Student, related_name='groups')
+    admin = models.ForeignKey(Admin, on_delete=models.CASCADE, related_name='groups')
+
     # علاقة Many-to-Many مع الطلاب (طالب يمكن أن ينتمي إلى عدة مجموعات)
-    
+
     def __str__(self):
         return f"Group {self.id_group} - {self.year_of_creation}"
