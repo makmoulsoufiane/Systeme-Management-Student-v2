59a462373a75a5ebbc0e6146f0d86a38bdfdb961
makmoul soufiane <makmoulsoufiane2@gmail.com>
2025-01-22 22:47:01 +0100

    relation ship last version

--- Group/models.py
+++ Group/models.py
@@ -1,6 +1,6 @@
 # Group/models.py
 from django.db import models
-from student.models import Student
+from Teachers.models import Teacher
 from Admin.models import Admin
 
 
@@ -8,8 +8,9 @@ from Admin.models import Admin
 class Group(models.Model):
     id_group = models.AutoField(primary_key=True)
     year_of_creation = models.PositiveIntegerField()
-    students = models.ManyToManyField(Student, related_name='groups')
     admin = models.ForeignKey(Admin, on_delete=models.CASCADE, related_name='groups')
+    teacher = models.OneToOneField(Teacher, on_delete=models.CASCADE, related_name='group')
+
 
     # علاقة Many-to-Many مع الطلاب (طالب يمكن أن ينتمي إلى عدة مجموعات)
 
