a329d0d3ebe5e59c5cb43bc23b1a7ca8f4a2e789
Boubaker17 <boubaker.benaissa@hotmail.fr>
2025-01-28 23:53:36 +0100

    add relationship in the Teacher and Score models

--- Teachers/models.py
+++ Teachers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from Admin.models import Admin  # Importing Admin model for the relationship
 
 
 class Teacher(models.Model):
@@ -6,6 +7,8 @@ class Teacher(models.Model):
     fullname = models.CharField(max_length=100)
     adress = models.CharField(max_length=255)
     date_of_inscription = models.DateField()
+    admin = models.ForeignKey(Admin, on_delete=models.SET_NULL, null=True, related_name="managed_teachers")  # Admin managing the teacher
+
 
     def __str__(self):
         return self.fullname
