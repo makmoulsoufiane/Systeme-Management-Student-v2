1f8a349242518679493cfce13934c401992ba8ab
makmoul soufiane <makmoulsoufiane2@gmail.com>
2025-02-06 21:41:53 +0100

    bugg model exam Exam

--- Scores/models.py
+++ Scores/models.py
@@ -2,25 +2,24 @@ from django.db import models
 from student.models import Student
 
 from Teachers.models import Teacher  # Importing Teacher model for the relationship
-from exam.models import exam  # Importing Exam model for the relationship
+from exam.models import Exam  # Importing Exam model for the relationship
 from django.utils.timezone import now
-from Admin.models import Admin 
+from Admin.models import Admin
 
 class Score(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='scores')
     teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True, related_name="scores_added")  # Teacher who added the score
-    exam = models.ForeignKey(exam, on_delete=models.CASCADE, related_name='scores', null=True, blank=True)  # Exam related to the score
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE, related_name='scores', null=True, blank=True)  # Exam related to the score
     admin = models.ForeignKey(Admin, on_delete=models.SET_NULL, null=True, related_name="approved_scores")  # Admin who approved or managed the score
 
     note = models.FloatField()
 
     created_at = models.DateTimeField(auto_now_add=True, null=True)  # To track when the score was created
 
-    admin = models.ForeignKey(Admin, on_delete=models.CASCADE, related_name='scores' , null=True, blank=True)  
-   
+    admin = models.ForeignKey(Admin, on_delete=models.CASCADE, related_name='scores' , null=True, blank=True)
+
 
 
 
     def __str__(self):
         return f"Score: {self.note} - {self.student.fullname} by {self.teacher.fullname if self.teacher else 'Unknown Teacher'}"
-  
--- student_exam/models.py
+++ student_exam/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from student.models import Student  # Import from student app
-from exam.models import exam  # Import from exam app
+from exam.models import Exam  # Import from exam app
 
 class StudentExam(models.Model):
     id = models.AutoField(primary_key=True)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    exam = models.ForeignKey(exam, on_delete=models.CASCADE)
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
     score = models.FloatField(null=True, blank=True)  # Optional field for scores
     created_at = models.DateTimeField(auto_now_add=True)  # Timestamp
 
