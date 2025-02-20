4afc12c987ac48556d8d4ad4edab8d03f0bcc326
makmoul soufiane <makmoulsoufiane2@gmail.com>
2025-02-20 17:30:14 +0100

    class based view students et studentdetail

--- student/urls.py
+++ student/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 from .import views
 
 urlpatterns = [
-    path('', views.student_list),
-    path('<int:id_student>/', views.student_detail),
+    path('', views.StudentList.as_view(), name='studentlist'),
+    path('<int:pk>/', views.StudentDetail.as_view(), name='studentdetail'),
+
 ]
--- student/views.py
+++ student/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from .models import Student  # Import the Student model
+from django.views.generic import ListView, DetailView
+
+
+class StudentList(ListView):
+    model = Student
+    # context_object_name = 'students'
+
+
+
+class StudentDetail(DetailView):
+    model = Student
 
 # Create your views here.
-def student_list(request):
-    student_list = Student.objects.all()  # Complete the query to fetch all students
-    context = {'students' : student_list}
-    return render(request, 'student/student_list.html', context)  # Pass the student list to the template
+# def student_list(request):
+#     student_list = Student.objects.all()  # Complete the query to fetch all students
+#     context = {'students' : student_list}
+#     return render(request, 'student/student_list.html', context)  # Pass the student list to the template
 
 
 
-def student_detail(request, id_student):
-    student_detail = Student.objects.get(id_student=id_student)  # Complete the query to fetch all students
-    context = {'student' : student_detail}
-    return render(request, 'student/student_detail.html', context)  # Pass the student list to the template
+# def student_detail(request, id_student):
+#     student_detail = Student.objects.get(id_student=id_student)  # Complete the query to fetch all students
+#     context = {'student' : student_detail}
+#     return render(request, 'student/student_detail.html', context)  # Pass the student list to the template
