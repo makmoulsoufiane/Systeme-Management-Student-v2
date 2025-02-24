7457eefc972b84f7178b8bd8eb9ceb81e09290d8
makmoul soufiane <makmoulsoufiane2@gmail.com>
2025-02-24 14:07:57 +0000

    add link all_student html

--- student/urls.py
+++ student/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from .import views
 
 urlpatterns = [
-    path('', views.StudentList.as_view(), name='studentlist'),
-    path('<int:pk>/', views.StudentDetail.as_view(), name='studentdetail'),
+    path('', views.StudentList.as_view(), name='student_list'),
+    path('<int:pk>/', views.StudentDetail.as_view(), name='student_detail'),
 
 ]
