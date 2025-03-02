44f93acb6019e1ca438ba1adf82cff2f180f0399
mariambenali <miriam.bena@gmail.com>
2025-03-02 00:50:45 +0000

    add templates for add delete and update, and also i modify urls and views

--- Admin/urls.py
+++ Admin/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from . views import AdminListView, AdminDetailView
+from . views import AdminCreateView, AdminDeleteView, AdminListView, AdminDetailView, AdminUpdateView
 
 
 urlpatterns = [
     path('', AdminListView.as_view(), name='admin_list'),
     path('<int:pk>/', AdminDetailView.as_view(), name='admin_detail'),
+    path('<int:pk>/create/', AdminCreateView.as_view(), name='admin_create'),
+    path('<int:pk>/update/', AdminUpdateView.as_view(), name='admin_update'),
+    path('<int:pk>/delete/', AdminDeleteView.as_view(), name='admin_delete'),
 ]
+
--- Admin/views.py
+++ Admin/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Admin
 
 class AdminListView(ListView):
@@ -7,3 +8,22 @@ class AdminListView(ListView):
 
 class AdminDetailView(DetailView):
     model = Admin
+    fields = ['fullname', 'address', 'date_of_inscription']
+
+
+class AdminCreateView(CreateView):
+    model = Admin
+    fields = ['fullname', 'address', 'date_of_inscription']
+    template_name = "Admin/admin_create.html"
+
+
+class AdminUpdateView(UpdateView):
+    model = Admin
+    fields = ['fullname', 'address', 'date_of_inscription']
+    template_name = "Admin/admin_update.html"
+
+
+class AdminDeleteView(DeleteView):
+    model = Admin
+    fields = ['fullname', 'address', 'date_of_inscription']
+    template_name = "Admin/admin_delete.html"
