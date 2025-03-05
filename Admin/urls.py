85eeeda7ceeb73d144ee7a85700c4ec7ec80b13b
mariambenali <miriam.bena@gmail.com>
2025-03-05 23:26:08 +0000

    add Admin_create template

--- Admin/urls.py
+++ Admin/urls.py
@@ -5,7 +5,7 @@ from . views import AdminCreateView, AdminDeleteView, AdminListView, AdminDetail
 urlpatterns = [
     path('', AdminListView.as_view(), name='admin_list'),
     path('<int:pk>/', AdminDetailView.as_view(), name='admin_detail'),
-    path('<int:pk>/create/', AdminCreateView.as_view(), name='admin_create'),
+    path('create/', AdminCreateView.as_view(), name='admin_create'), 
     path('<int:pk>/update/', AdminUpdateView.as_view(), name='admin_update'),
     path('<int:pk>/delete/', AdminDeleteView.as_view(), name='admin_delete'),
 ]
--- Admin/views.py
+++ Admin/views.py
@@ -1,4 +1,6 @@
 
+from pyexpat.errors import messages
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Admin
 
@@ -15,6 +17,11 @@ class AdminCreateView(CreateView):
     model = Admin
     fields = ['fullname', 'address', 'date_of_inscription']
     template_name = "Admin/admin_create.html"
+    success_url = reverse_lazy('admin_list')  
+
+    def form_valid(self, form):
+        messages.success(self.request, "Admin created successfully!")  
+        return super().form_valid(form) 
 
 
 class AdminUpdateView(UpdateView):
@@ -27,3 +34,11 @@ class AdminDeleteView(DeleteView):
     model = Admin
     fields = ['fullname', 'address', 'date_of_inscription']
     template_name = "Admin/admin_delete.html"
+
+
+
+'''
+class AdminCreateStudent(AdminCreateStudentView):
+    model = Admin
+    fields = ['fullname', 'address', 'date_of_inscription']
+    template_name = "Admin/admin_create_student.html" '''
