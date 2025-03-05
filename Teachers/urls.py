aa9b279a9e5267240589cbedf0043565b5f1809d
unknown <boubaker.benaissa@hotmail.fr>
2025-03-05 02:15:04 +0100

    create score and exam template (create, edit, delete) for teacher details page

--- Teachers/urls.py
+++ Teachers/urls.py
@@ -1,11 +1,21 @@
 
 # teachers/urls.py
 from django.urls import path
-from .views import TeacherListView, TeacherDetailView  
+from .views import (TeacherListView, TeacherDetailView, ScoreCreateView, ScoreUpdateView, ScoreDeleteView, 
+    ExamCreateView, ExamUpdateView, ExamDeleteView) 
 
 urlpatterns = [
     path('', TeacherListView.as_view(), name='teacher_list'),
     path('<int:pk>/', TeacherDetailView.as_view(), name='teacher_detail'),
+    # Score URLs
+    path('<int:teacher_id>/add-score/', ScoreCreateView.as_view(), name='add_score'),
+    path('update-score/<int:pk>/', ScoreUpdateView.as_view(), name='update_score'),
+    path('delete-score/<int:pk>/', ScoreDeleteView.as_view(), name='delete_score'),
+
+    # Exam URLs
+    path('<int:teacher_id>/add-exam/', ExamCreateView.as_view(), name='add_exam'),
+    path('update-exam/<int:pk>/', ExamUpdateView.as_view(), name='update_exam'),
+    path('delete-exam/<int:pk>/', ExamDeleteView.as_view(), name='delete_exam'),
 ]
 
 
--- Teachers/views.py
+++ Teachers/views.py
@@ -1,6 +1,12 @@
 # teachers/views.py
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy # creates a URL only when it's needed, not immediately
+from django.shortcuts import get_object_or_404 # it finds an object in the database and automatically shows a "404 Not Found" error if it not found
 from .models import Teacher
+from student.models import Student # student model
+from exam.models import Exam # exam model
+from Scores.models import Score # score model
+
 
 class TeacherListView(ListView):
     model = Teacher
@@ -11,6 +17,80 @@ class TeacherDetailView(DetailView):
     model = Teacher
     template_name = "teachers/teacher_detail.html"
     context_object_name = "teacher"
+
+# Score Views
+class ScoreCreateView(CreateView):
+    model = Score
+    fields = ['student', 'exam', 'note']
+    template_name = "teachers/score_form.html"
+
+    def form_valid(self, form):
+        teacher_id = self.kwargs['teacher_id']
+        teacher = get_object_or_404(Teacher, id_teacher=teacher_id)
+        form.instance.teacher = teacher
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['teacher_id'] = self.kwargs['teacher_id']  # Send teacher_id to the template
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('teacher_detail', kwargs={'pk': self.kwargs['teacher_id']})
+
+
+class ScoreUpdateView(UpdateView):
+    model = Score
+    fields = ['note']
+    template_name = "teachers/score_form.html"
+
+    def get_success_url(self):
+        return reverse_lazy('teacher_detail', kwargs={'pk': self.object.teacher.id_teacher})
+
+
+class ScoreDeleteView(DeleteView):
+    model = Score
+    template_name = "teachers/score_confirm_delete.html"
+
+    def get_success_url(self):
+        return reverse_lazy('teacher_detail', kwargs={'pk': self.object.teacher.id_teacher})
+
+# Exam Views
+class ExamCreateView(CreateView):
+    model = Exam
+    fields = ['date_hour', 'place', 'name', 'group']
+    template_name = "teachers/exam_form.html"
+
+    def form_valid(self, form):
+        teacher_id = self.kwargs['teacher_id']
+        teacher = get_object_or_404(Teacher, id_teacher=teacher_id)
+        form.instance.teacher = teacher
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['teacher_id'] = self.kwargs['teacher_id']  # Send teacher_id to the template
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('teacher_detail', kwargs={'pk': self.kwargs['teacher_id']})
+
+
+class ExamUpdateView(UpdateView):
+    model = Exam
+    fields = ['date_hour', 'place', 'name']
+    template_name = "teachers/exam_form.html"
+
+    def get_success_url(self):
+        return reverse_lazy('teacher_detail', kwargs={'pk': self.object.teacher.id_teacher})
+
+
+class ExamDeleteView(DeleteView):
+    model = Exam
+    template_name = "teachers/exam_confirm_delete.html"
+
+    def get_success_url(self):
+        return reverse_lazy('teacher_detail', kwargs={'pk': self.object.teacher.id_teacher})
 
 
 
