ae1c11e6aa88f6fcb19875248f5fd3d01ed70373
makmoul soufiane <makmoulsoufiane2@gmail.com>
2025-03-07 23:05:45 +0000

    add comment to js file to understand event listen code

--- student/static/js/student.js
+++ student/static/js/student.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Select all elements with the class 'student-card'
   let cards = document.querySelectorAll(".student-card");
 
+  // Iterate over each card
   cards.forEach(function (card) {
-      card.addEventListener("click", function () {
-          // Prevent navigation when clicking the button
+    // Add a click event listener to each card
+    card.addEventListener("click", function () {
+      // Get the URL from the data-url attribute of the card
+      let url = card.getAttribute("data-url");
 
-              let url = card.getAttribute("data-url");
-              if (url) {
-                  window.location.href = url;
-              }
-          }
-      );
+      // If the URL exists, navigate to it
+      if (url) {
+        window.location.href = url;
+      }
+    });
   });
 });
