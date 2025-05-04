e81339955e54fb4960066a1fd32942440846ae63
makmoul soufiane <makmoulsoufiane2@gmail.com>
2025-05-04 14:09:14 +0100

    add slug in model accounts

--- accounts/models.py
+++ accounts/models.py
@@ -4,12 +4,12 @@ from django.utils import timezone
 from django.urls import reverse
 from django.utils.translation import gettext as _
 from django_countries.fields import CountryField
+from django.utils.text import slugify
 
 # Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, verbose_name=_("Name"))
     slug = models.SlugField(max_length=100, unique=True, blank=True, null=True, verbose_name=_("Slug"))
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True, verbose_name=_("Profile Picture"))
     country = CountryField(blank=True, null=True, verbose_name=_("Country"))
@@ -17,12 +17,19 @@ class Profile(models.Model):
     join_date = models.DateTimeField(default=timezone.now, verbose_name=_("Join Date"))
 
 
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.user.username)
+        super(Profile, self).save(*args, **kwargs)
+
+
     class Meta:
         verbose_name = _("Profile")
         verbose_name_plural = _("Profiles")
 
     def __str__(self):
-        return '%s' %(self.name)
+        return '%s' % (self.user.username)
 
     def get_absolute_url(self):
         return reverse("accounts:Profile_detail", kwargs={"slug": self.slug})
