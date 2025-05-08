95d80a7707c12eefe9094335248aa2216a8bfe33
mariambenali <miriam.bena@gmail.com>
2025-05-08 22:45:32 +0100

    add a new branch for login page

--- sms/settings.py
+++ sms/settings.py
@@ -44,6 +44,12 @@ INSTALLED_APPS = [
     'Scores',
     'Group',
     'student_exam',
+    'userauth',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     ]
 
 
@@ -143,3 +149,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'dashboard'  
+LOGOUT_REDIRECT_URL = 'login'
